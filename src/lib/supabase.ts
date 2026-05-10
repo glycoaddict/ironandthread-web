e171bf97966f82f3c0be55ba522574b0e3bb24b2
Matthew Choo <choo.matt@gmail.com>
2026-05-11 00:17:50 +0800

    Initial commit from Create Next App

---
+++ src/lib/supabase.ts
@@ -0,0 +1,18 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const createClerkSupabaseClient = (session: any) => {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      global: {
+        fetch: async (url, options = {}) => {
+          const clerkToken = await session?.getToken({ template: 'supabase' });
+          const headers = new Headers(options?.headers);
+          headers.set('Authorization', `Bearer ${clerkToken}`);
+          return fetch(url, { ...options, headers });
+        },
+      },
+    }
+  );
+};
